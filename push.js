7aa2b5fa06b45c1bbc17129af56474af49f7faa5
funXMutulay <mkebe1@yhoo.fr>
2022-06-28 12:42:00 -0700

    fix

--- push.js
+++ push.js
@@ -4,10 +4,22 @@ var  push = require ('web-push')
 let vapIdKeys =  {
     publicKey: 'BFEtXeBEvNpbtvNS2sw7wFhL4cwn30cJzrX8Af5TcO4YSku4-ZaRqfg6DZ9eE1hsLP7bywehwxq_gr1QKP-V6j0',
     privateKey: 'eRLoXJ4th2DEHEibdcXd1PNtmcVDU5xdcjlvAxQTQCs'
-  }
+  };
 
-  push.setVapidDetails('mailto:mmdukb@hotmail.com',vapIdKeys.publicKey,vapIdKeys.privateKey)
+  push.setVapidDetails(
+    'mailto:mmdukb@hotmail.com',
+    vapIdKeys.publicKey,
+    vapIdKeys.privateKey
+    );
 
-let sub = {} ;
+let sub ={endpoint:
+            "https://fcm.googleapis.com/fcm/send/eo6WvHzUkE8:APA91bH7akoE-3DEGmdLhnaXyEFVhLNa12IiaJ8JFU9u1c7tEv0NBv-sta99xsUR3Np_m2Xpsg5m2EUJH6YKEufl2Tqw9xbk3p9axXnvWwTpLFxsOtWnr6cxvcLNMXfukWm6oTuApn7c",
+          expirationTime:null,
+          keys:{
+            p256dh:
+            "BGKen4URVhVmtxb5gOx1EQgrr32Ga_aBuymBpEjIEb7WDo5maQyuLDoiWYmXabfzTRkasUpvJulvdEZQeg40AUw",
+            auth:"juo_27X9kRe4qLlNEw1-hA"
+                }
+          } ;
 
   push.sendNotification(sub,'test message')
--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
 self.addEventListener('push', () => {
+
+
+
+
 self.ServiceWorkerRegistration.sendNotifications('test message',{});        
 } )
+
+self.addEventListener('push',function(e){
+    var options ={
+        body: 'This notification was generated from a push ' ,
+        icon: 'images/example.png' ,
+        vibrate: {100. 50 100} ,
+        data: {
+            dateOfArrival: Date.now() ,
+            primaryKey:'2'
+        },
+        actions:[
+            {action: 'explore' ,title: 'explore this new world',
+            icon: 'images/checkmark.png'},
+            {action:'close', title : 'Close',icon:'image/mark.png'}
+        }
+    ];
+    e.waiUntil(
+        self.ServiceWorkerRegistration.showNotification('Hello World',options)
+    );
+
+});
