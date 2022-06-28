5e7a19e66e7adba2fe772a736c19c2c4bdf4b767
funXMutulay <mkebe1@yhoo.fr>
2022-06-28 14:11:53 -0700

    fix

--- sw.js
+++ sw.js
@@ -1,22 +1,23 @@
 self.addEventListener('push',function(e){
-    var options ={
+    var options = {
         body: 'This notification was generated from a push ' ,
         icon: 'images/example.png' ,
         vibrate: [100, 50 , 100] ,
         data: {
+
             dateOfArrival: Date.now() ,
             primaryKey:'2'
-        },
+                },
         actions:[
-            {action: 'explore' ,title: 'explore this new world',
-            icon: 'images/checkmark.png'},
+            {
+                action: 'explore' ,
+                title: 'explore this new world',
+                icon: 'images/checkmark.png'
+        },
             
-            {action:'close', title : 'Close',
-            icon:'image/xmark.png'}
+            {action:'close', title : 'Close', icon:'image/xmark.png'}
         ]
     };
-    e.waiUntil(
-        self.ServiceWorkerRegistration.showNotification('Hello World',options)
-    );
+    e.waitUntil(self.registration.showNotification('Hello World',options));
 
 });
